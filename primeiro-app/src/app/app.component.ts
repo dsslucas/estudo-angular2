d68115b98ece75dc5fa9b2dd7a412595800a3c6d
Lucas Silva <lucas_silva_souza@hotmail.com>
2022-03-22 13:48:49 -0300

    first commit

---
+++ primeiro-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+//O que o HTML base consulta
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'primeiro-app';
+}
