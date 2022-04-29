33fd322ac4c7d34cdc2733456cb32d2a1b982f64
Lucas Silva <lucas_silva_souza@hotmail.com>
2022-04-29 15:16:51 -0300

    Configuração dos módulos e páginas

--- pokedex/src/app/app-routing.module.ts
+++ pokedex/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+//Usa Lazy Loading
+const routes: Routes = [
+  {path: '', loadChildren: () => import('./pages/pages.module').then(p => p.PagesModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ pokedex/src/app/pages/routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+
+//Vem do Lazy Loading. AQUI É FILHO@
+const routes: Routes = [
+  {path: '', component: HomeComponent}, //Página principal
+  {path: 'details', component: DetailsComponent} // Detalhes
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class RoutingModule { }
