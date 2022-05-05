352fda639d3174d6b730c4b71192f2ac5f590433
Lucas Silva <lucas_silva_souza@hotmail.com>
2022-05-05 18:09:13 -0300

    Início aos testes de interface

--- teste-angular/src/app/shared/banking/banking.component.spec.ts
+++ teste-angular/src/app/shared/banking/banking.component.spec.ts
@@ -8,9 +8,9 @@ describe('BankingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BankingComponent ]
+      declarations: [BankingComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -38,6 +38,17 @@ describe('BankingComponent', () => {
     expect(component.getCarteira).toEqual(60)
   })
 
+  //Teste nos IFs
+  it('(Unidade) setSacar(): should transfer poupanca dont have string (isNaN) oR poupanca < value', () => {
+    expect(component.setSacar('string')).not.toBeTruthy() //isNaN
+    expect(component.setSacar('100')).not.toBeTruthy; //Valor acima
+
+    expect(component.getPoupanca).toEqual(10)
+    expect(component.getCarteira).toEqual(50)
+
+  })
+
+
   it('(Unitário) setDepositar: should transfer carteira from poupanca', () => {
     component.setDeposito('50')
     //fixture.detectChanges()
@@ -45,6 +56,48 @@ describe('BankingComponent', () => {
     expect(component.getCarteira).toEqual(0)
     expect(component.getPoupanca).toEqual(60)
 
+  })
+
+  //Teste nos IFs
+  it('(Unidade) setDeposito(): should transfer carteira dont have string (isNaN) oR CARTEIRA < value', () => {
+    expect(component.setDeposito('string')).not.toBeTruthy() //isNaN
+    expect(component.setDeposito('100')).not.toBeTruthy; //Valor acima
+
+    expect(component.getPoupanca).toEqual(10)
+    expect(component.getCarteira).toEqual(50)
 
   })
+
+  // TESTES DE TELA - TESTES DE TELA - TESTES DE TELA - TESTES DE TELA - TESTES DE TELA
+
+  it('(iNTERFACE) setDeposito(): should transfer carteira to Poupanca', () => {
+    let el = fixture.debugElement.nativeElement
+    el.querySelector('#input-depositar').value = '10'
+    el.querySelector('#depositar').click();
+
+    //Detecta alterações
+    fixture.detectChanges()
+
+    expect(el.querySelector('#get-poupanca').textContent).toEqual('20')
+
+    //expect(component.getPoupanca).toEqual(20)
+    //expect(component.getCarteira).toEqual(40)
+
+  })
+
+  it('(iNTERFACE) setSacar(): should transfer Poupanca to Carteira', () => {
+    let el = fixture.debugElement.nativeElement
+    el.querySelector('#input-sacar').value = '10'
+    el.querySelector('#sacar').click();
+
+    //Detecta alterações
+    fixture.detectChanges()
+
+    expect(el.querySelector('#get-carteira').textContent).toEqual('60')
+
+    //expect(component.getPoupanca).toEqual(20)
+    //expect(component.getCarteira).toEqual(40)
+
+  })
+
 });
